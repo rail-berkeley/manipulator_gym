8df2cdcf6779eb1b36bf1a11dbcbc0b8f6e0d03b
youliangtan <tan_you_liang@hotmail.com>
2024-07-15 20:20:11 -0700

    support widowx
    Signed-off-by: youliangtan <tan_you_liang@hotmail.com>

--- manipulator_gym/interfaces/widowx.py
+++ manipulator_gym/interfaces/widowx.py
@@ -3,6 +3,7 @@ from sensor_msgs.msg import Image
 
 import numpy as np
 import math
+import cv2
 
 from manipulator_gym.utils.utils import rotationMatrixToEulerAngles
 from manipulator_gym.interfaces.viperx import ViperXInterface
@@ -17,25 +18,78 @@ class WidowXInterface(ViperXInterface):
     https://github.com/Interbotix/interbotix_ros_toolboxes/blob/main/interbotix_xs_toolbox/interbotix_xs_modules/src/interbotix_xs_modules/arm.py   
     """
 
-    def __init__(self, init_node=True, blocking_control=True):
+    def __init__(self, init_node=True, blocking_control=True, cam_ids=None):
+        """
+        Args:
+            cam_ids (list): list of camera ids to use for the interface,
+                if None, use ROS subscribers to get images
+        """
         self._bot = InterbotixManipulatorXS(
             "wx250s", "arm", "gripper", init_node=init_node)
         self._arm = self._bot.arm
         self._gripper = self._bot.gripper
-        self._cam_sub1 = rospy.Subscriber(
-            "/gripper_cam/image_raw", Image, self._update_wrist_cam)
-        self._cam_sub2 = rospy.Subscriber(
-            "/third_perp_cam/image_raw", Image, self._update_primary_cam)
+
+        # user have the option to use rossub or native opencv to read camera
+        if cam_ids is None:
+            self._cam_sub1 = rospy.Subscriber(
+                "/gripper_cam/image_raw", Image, self._update_wrist_cam)
+            self._cam_sub2 = rospy.Subscriber(
+                "/third_perp_cam/image_raw", Image, self._update_primary_cam)
+            self._caps = None
+        else:
+            print("Using camera ids: ", cam_ids)
+            assert len(cam_ids) <= 2, "Only 2 cameras are supported"
+            for id in cam_ids:
+                self._caps.append(cv2.VideoCapture(id))
+
         self._side_img = np.array((480, 640, 3), dtype=np.uint8)
         self._wrist_img = np.array((480, 640, 3), dtype=np.uint8)
         self.blocking_control = blocking_control
 
-    def reset(self, reset_pose=True, go_sleep=False) -> bool:
-        """Override function from base class"""
+    @property
+    def primary_img(self) -> np.ndarray:
+        if self._caps is not None:
+            return self._side_img
+        else:
+            ret, frame = self._caps[0].read()
+            # If frame is read correctly ret is True
+            if not ret:
+                raise Exception(
+                    "Can't receive frame (stream end?). Exiting ...")
+            return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    @property
+    def wrist_img(self):
+        if self._caps is not None:
+            return self._wrist_img
+        elif len(self._caps) < 2:  # no idx 1
+            return None
+        else:
+            ret, frame = self._caps[1].read()
+            # If frame is read correctly ret is True
+            if not ret:
+                raise Exception(
+                    "Can't receive frame (stream end?). Exiting ...")
+            return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    def reset(self,
+              reset_pose=True,
+              target_state=np.array(
+                  np.array([0.258325, 0, 0.19065, 0, math.pi/2, 0, 1.0])),
+              go_sleep=False
+              ) -> bool:
+        """
+        Override function from base class
+        Default reset position is home pose, with open gripper
+        """
         print("Reset robot interface, reset to home pose?: ", reset_pose)
         if reset_pose:
-            self.move_eef(np.array([0.258325, 0, 0.19065, 0, math.pi/2, 0]))
-            self._gripper.open()
+            print("Moving to target state: ", target_state)
+            if target_state[6] > 0.5:
+                self._gripper.open(delay=0.1)
+            else:
+                self._gripper.close(delay=0.1)
+            self.move_eef(target_state[:6])
             if go_sleep:
                 self._arm.go_to_sleep_pose()
         return True
